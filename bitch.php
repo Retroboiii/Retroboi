f4e5d0358022c09525889f4b8f8550af32088166
Retroboiii <99901347+Retroboiii@users.noreply.github.com>
2022-02-22 22:04:58 +0300

    Add files via upload

---
+++ bitch.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+i = rand(int 0, int 10): int 
+if(i < 5)
+{
+    echo "Call him a Bitch";
+}
+elseif(i > 5)
+{
+    echo "Call him a Fren";
+}
+
+
+?>
+This website is bcs fuck you idk what to put here 
+</body>
+</html>
